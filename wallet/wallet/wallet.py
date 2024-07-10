a95dc0c04cbe795c46d623f9401f24bf4f5109b9
arthur.jovita <arthur.jovita@adevinta.com>
2024-07-10 22:52:45 +0200

    feat: implement withdrawal feature

--- wallet/wallet/wallet.py
+++ wallet/wallet/wallet.py
@@ -7,5 +7,12 @@ class Wallet:
   def check_balance(self):
     return self.balance
   
+  def withdraw(self, amount):
+    if amount <= 0:
+      raise ValueError('Amount must be positive')
+    if amount > self.balance:
+      raise ValueError('Amount must be less than the balance')
+    self.balance -= amount
+  
   def __str__(self):
     return f'Wallet of {self.owner} with balance of {self.balance} {self.currency}'
