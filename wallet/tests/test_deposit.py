fde4bbe9f091825d4610660e2a10dcbf258487f2
Arthur Guerra <31071457+arthurjguerra@users.noreply.github.com>
2024-07-10 22:33:38 +0200

    feat: implement deposit feature (#4)

--- wallet/tests/test_deposit.py
+++ wallet/tests/test_deposit.py
@@ -9,7 +9,12 @@ class TestDeposit(unittest.TestCase):
                               initial_balance=1000, owner='Test User')
 
   def test_deposit(self):
-    pass
+    self.test_wallet.deposit(500)
+    self.assertEqual(self.test_wallet.check_balance(), 1500)
+
+  def test_deposit_negative(self):
+    with self.assertRaises(ValueError):
+      self.test_wallet.deposit(-500)
 
 
 if __name__ == '__main__':
