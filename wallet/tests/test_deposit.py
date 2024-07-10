cea85cfd376b6085a957a15a2ff18b872bd4e521
arthur.jovita <arthur.jovita@adevinta.com>
2024-07-10 21:26:40 +0200

    feat: update tests

--- wallet/tests/test_deposit.py
+++ wallet/tests/test_deposit.py
@@ -8,12 +8,8 @@ class TestDeposit(unittest.TestCase):
     self.test_wallet = Wallet(currency=Currency.EUR,
                               initial_balance=1000, owner='Test User')
 
-  def test_initial_balance(self):
-    self.assertEqual(self.test_wallet.check_balance(), 1000)
-
-  def test_wallet_to_str(self):
-    self.assertEqual(str(self.test_wallet),
-                     'Wallet of Test User with balance of 1000 EUR')
+  def test_deposit(self):
+    pass
 
 
 if __name__ == '__main__':
