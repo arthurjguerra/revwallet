605dca3bffda5e49580608b01944fe14d14f00fc
arthur.jovita <arthur.jovita@adevinta.com>
2024-07-24 11:13:08 +0200

    feat: increase test coverage

--- tests/test_check_balance.py
+++ tests/test_check_balance.py
@@ -9,18 +9,34 @@ class TestCheckBalance(unittest.TestCase):
   def test_initial_balance(self):
     self.assertEqual(self.test_wallet.check_balance(), '1000 EUR')
 
-  def test_deposit(self):
+  def test_check_balance_after_deposit(self):
     self.test_wallet.deposit(500)
     self.assertEqual(self.test_wallet.check_balance(), '1500 EUR')
 
-  def test_withdraw(self):
+  def test_check_balance_after_withdraw(self):
     self.test_wallet.withdraw(200.50)
     self.assertEqual(self.test_wallet.check_balance(), '799.5 EUR')
 
-  def test_multiple_transactions(self):
+  def test_check_balance_after_multiple_transactions(self):
       self.test_wallet.withdraw(100)
       self.test_wallet.deposit(155.55)
       self.assertEqual(self.test_wallet.check_balance(), '1055.55 EUR')
+
+  def test_check_balance_after_invalid_deposit(self):
+    try:
+      self.test_wallet.deposit(-100)
+    except Exception:
+      pass
+    finally:
+      self.assertEqual(self.test_wallet.check_balance(), '1000 EUR')
+
+  def test_check_balance_after_invalid_withdraw(self):
+    try:
+      self.test_wallet.withdraw(2222222)
+    except Exception:
+      pass
+    finally:
+      self.assertEqual(self.test_wallet.check_balance(), '1000 EUR')
     
 
 if __name__ == '__main__':
