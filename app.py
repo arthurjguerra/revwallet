3ccec79482d736b25be4417d7ba26d386226572d
arthur.jovita <arthur.jovita@adevinta.com>
2024-07-26 22:38:05 +0200

    fix: update grafana config and dashboard

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = create_app()
 metrics = PrometheusMetrics(app)
 
-metrics.info('app_info', 'Application info', version='v0.2.0')
+metrics.info('app_info', 'revwallet', version='v0.2.0')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
