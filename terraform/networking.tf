cf450c7c49114f97ed00db66acf0e804ddf68cc3
arthur.jovita <arthur.jovita@adevinta.com>
2024-09-01 22:39:25 +0200

    feat: implement cloud infra

---
+++ terraform/networking.tf
@@ -0,0 +1,38 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "revwallet-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "nat_gateway_ids" {
+  value = module.vpc.nat_gateway_ids
+}
+
+output "vpn_gateway_id" {
+  value = module.vpc.vpn_gateway_id
+}
